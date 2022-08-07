d986ca5bb666acc23a9d8b844a5896413f6f8dcd
Souleymane Mouctar Diallo <soulmouctar96@gmail.com>
2022-08-07 21:03:05 +0000

    bugg fixing

--- src/app/assurance/add/add.component.ts
+++ src/app/assurance/add/add.component.ts
@@ -99,28 +99,4 @@ export class AddComponent implements OnInit {
     this.router.navigateByUrl('/detail');
   }
 
-
-  // static dateMinimum(date: string): ValidatorFn {
-  //   return (control: AbstractControl): ValidationErrors | null => {
-  //     if (control.value == null) {
-  //       return null;
-  //     }
-
-  //     const controlDate = moment(control.value, FORMAT_DATE);
-
-  //     if (!controlDate.isValid()) {
-  //       return null;
-  //     }
-
-  //     const validationDate = moment(date);
-
-  //     return controlDate.isAfter(validationDate) ? null : {
-  //       'date-minimum': {
-  //         'date-minimum': validationDate.format(FORMAT_DATE),
-  //         'actual': controlDate.format(FORMAT_DATE)
-  //       }
-  //     };
-  //   };
-  // }
-
 }
